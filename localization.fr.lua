f5021577205d1631ffb7e06d286fac6c9c0f2296
Fragglechen <159826876+Fragglechen@users.noreply.github.com>
2026-05-04 22:46:44 +0200

    Initial EpochMail port

---
+++ localization.fr.lua
@@ -0,0 +1,12 @@
+EpochMail = EpochMail or {}
+local L = EpochMail.L
+
+if GetLocale() == "frFR" then
+  L[ "collected" ]      = "collectÃ©"
+  L[ "1st mail" ]       = "1er message"
+  L[ "each mail" ]      = "chaque message"
+  L[ "Money received" ] = "Argent reÃ§u"
+  L[ "All mails" ]      = "Tous mails"
+  L[ "This mail was returned to you." ] = "Ce courrier vous a Ã©tÃ© retournÃ©."
+  L[ "This mail was sent by an auctionhouse." ] = "Ce courrier a Ã©tÃ© envoyÃ© par une maison de vente aux enchÃ¨res."
+end
---
+++ localization.ru.lua
@@ -0,0 +1,12 @@
+EpochMail = EpochMail or {}
+local L = EpochMail.L
+
+if GetLocale() == "ruRU" then
+  L[ "collected" ]      = "ÑÐ¾Ð±Ñ€Ð°Ð½Ð¾"
+  L[ "1st mail" ]       = "1-e Ð¿Ð¸ÑÑŒÐ¼Ð¾"
+  L[ "each mail" ]      = "ÐšÐ°Ð¶Ð´Ð¾Ðµ Ð¿Ð¸ÑÑŒÐ¼Ð¾"
+  L[ "Money received" ] = "Ð”ÐµÐ½ÑŒÐ³Ð¸ Ð¿Ð¾Ð»ÑƒÑ‡ÐµÐ½Ñ‹"
+  L[ "All mails" ]      = "Bce Ð¿Ð¸ÑÑŒÐ¼Ð°"
+  L[ "This mail was returned to you." ] = "Ð­Ñ‚Ð¾ Ð¿Ð¸ÑÑŒÐ¼Ð¾ Ð±Ñ‹Ð»Ð¾ Ð²Ð¾Ð·Ð²Ñ€Ð°Ñ‰ÐµÐ½Ð¾ Ð²Ð°Ð¼."
+  L[ "This mail was sent by an auctionhouse." ] = "Ð­Ñ‚Ð¾ Ð¿Ð¸ÑÑŒÐ¼Ð¾ Ð±Ñ‹Ð»Ð¾ Ð¾Ñ‚Ð¿Ñ€Ð°Ð²Ð»ÐµÐ½Ð¾ Ð°ÑƒÐºÑ†Ð¸Ð¾Ð½Ð½Ñ‹Ð¼ Ð´Ð¾Ð¼Ð¾Ð¼."
+end
